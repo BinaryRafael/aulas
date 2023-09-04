3814bdcc32f9e3250bed366bb5e5a5857ec665df
Administrador <rafael.gomes.oliver@outlook.com>
2023-09-04 11:04:59 -0300

    Initial commit

---
+++ src/main/java/org/ebac/Main.java
@@ -0,0 +1,12 @@
+package org.ebac;
+
+import org.ebac.aulas.ExercicioModulo6;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ExercicioModulo6 exercicio = new ExercicioModulo6();
+        exercicio.acordar(false);
+
+    }
+}
---
+++ src/main/java/org/ebac/aulas/ExercicioModulo6.java
@@ -0,0 +1,22 @@
+package org.ebac.aulas;
+
+public class ExercicioModulo6 {
+
+    public void acordar(boolean dormindo){
+
+        // Passo 2: Verifique se você está dormindo
+        if (dormindo) {
+            // Passo 3: Se estiver dormindo, acorde
+            System.out.println("Você está dormindo. Hora de acordar!");
+
+            // Passo 4: Levante-se da cama
+            System.out.println("Levante-se da cama.");
+
+        } else {
+            // Passo 6: Se você já está acordado, não precisa fazer nada
+            System.out.println("Você já está acordado. Não é preciso levantar da cama novamente.");
+        }
+    }
+
+
+}
